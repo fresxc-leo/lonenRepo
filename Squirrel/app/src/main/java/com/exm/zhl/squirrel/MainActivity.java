cabb594f77d2c1cc14f87fe9ee3dd3cedd7792cd
lonen <494669579@qq.com>
2020-09-07 15:35:16 +0800

    Kagirl v1.1.1 add for MovingGirl of FrameLayout

--- Squirrel/app/src/main/java/com/exm/zhl/squirrel/MainActivity.java
+++ Squirrel/app/src/main/java/com/exm/zhl/squirrel/MainActivity.java
@@ -1,10 +1,17 @@
 package com.exm.zhl.squirrel;
 
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FrameLayout frame = (FrameLayout)findViewById(R.id.mylayout);
+/*        FrameLayout frame = (FrameLayout)findViewById(R.id.mylayout);
         final Kagirl kag = new Kagirl(MainActivity.this);
         kag.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -24,5 +31,65 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         frame.addView(kag);
+*/
+        frame = (FrameLayout)findViewById(R.id.myframe);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.sendEmptyMessage(0x123);
+            }
+        }, 0, 170);
+    }
+    FrameLayout frame = null;
+    Handler handler = new Handler() {
+        int i = 0;
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what == 0x123) {
+                i++;
+                move(i % 8);
+            }
+            super.handleMessage(msg);
+        }
+    };
+
+    void move(int i)
+    {
+        Drawable a = ResourcesCompat.getDrawable(getResources(), R.drawable.s_1,null);
+        Drawable b = ResourcesCompat.getDrawable(getResources(), R.drawable.s_2,null);
+        Drawable c = ResourcesCompat.getDrawable(getResources(), R.drawable.s_3,null);
+        Drawable d = ResourcesCompat.getDrawable(getResources(), R.drawable.s_4,null);
+        Drawable e = ResourcesCompat.getDrawable(getResources(), R.drawable.s_5,null);
+        Drawable f = ResourcesCompat.getDrawable(getResources(), R.drawable.s_6,null);
+        Drawable g = ResourcesCompat.getDrawable(getResources(), R.drawable.s_7,null);
+        Drawable h = ResourcesCompat.getDrawable(getResources(), R.drawable.s_8,null);
+        switch (i)
+        {
+            case 0:
+                frame.setForeground(a);
+                break;
+            case 1:
+                frame.setForeground(b);
+                break;
+            case 2:
+                frame.setForeground(c);
+                break;
+            case 3:
+                frame.setForeground(d);
+                break;
+            case 4:
+                frame.setForeground(e);
+                break;
+            case 5:
+                frame.setForeground(f);
+                break;
+            case 6:
+                frame.setForeground(g);
+                break;
+            case 7:
+                frame.setForeground(h);
+                break;
+        }
     }
 }
